dcdbcdb7f15cbc0553b434f34bf9e09b12dd7868
rayyan-innova360 <muhammadrayyan@innova360.io>
2025-10-10 21:09:49 +0500

    Completed Industries Marquee section

--- src/components/Home/Industries.jsx
+++ src/components/Home/Industries.jsx
@@ -2,22 +2,18 @@ import IndustriesMarquee from "./IndustriesMarquee";
 
 const Industries = () => {
   return (
-    <div className="w-[100vw] bg-[linear-gradient(180deg,_#FFFFFF_0%,_#D5ECF0_115.91%)] min-h-[500px] flex flex-col items-center justify-center text-center">
+    <div className="w-[100vw]  bg-[linear-gradient(180deg,#FFFFFF_0%,#D5ECF0_115.91%)] min-h-[500px] flex flex-col items-center justify-center text-center ">
       {/* Heading */}
       <div className="flex flex-col items-center justify-center text-center px-4 py-10 sm:py-16 lg:py-24 gap-2">
-        <div
-          style={{ fontFamily: "'Lora', serif" }}
-          className="inline-flex items-center justify-center border font-lora font-medium text-[16px] leading-[100%] px-4 py-1 rounded-[7px] text-[#18BDA0]"
-        >
+        <div className="inline-flex items-center justify-center font-heading border font-medium text-[16px] leading-[100%] px-4 py-1 rounded-[7px] text-text">
           Discover Accurack
         </div>
-        <h1
-          style={{ fontFamily: "'Lora', serif" }}
-          className="text-gray-900 text-lg sm:text-3xl md:text-4xl font-semibold max-w-3xl"
-        >
+
+        <h1 className="text-text font-heading text-lg sm:text-3xl md:text-4xl font-semibold max-w-5xl">
           Our Integrations Power Diverse Industries
         </h1>
-        <p className="text-gray-400 text-sm sm:text-3xl md:text-sm max-w-xl">
+
+        <p className="text-light font-body text-sm sm:text-base md:text-sm max-w-xl">
           Accurack integrates with the tools you already use, streamlining
           operations across ecommerce, wholesale, logistics, and beyond.
         </p>
@@ -25,18 +21,13 @@ const Industries = () => {
 
       <IndustriesMarquee />
 
-      <button
-        style={{
-          fontFamily: "'Lora', serif",
-        }}
-        className="p-4 cursor-pointer w-60 border border-[#fffff] rounded-full mt-10 mb-20 bg-[#18BDA0] hover:bg-[#0f9e87] transition-colors duration-300 flex items-center justify-center gap-3"
-      >
-        <span className="text-white text-sm sm:text-lg md:text-sm font-medium">
-          Explore All Integrations
+      <button className="p-4 cursor-pointer w-60 border border-background bg-gradient-to-r from-gradient-primary-1 to-gradient-primary-2     rounded-full mt-10 mb-20   flex items-center justify-center gap-3 font-body">
+        <span className="text-background text-sm sm:text-lg md:text-sm font-medium">
+          View All Integrations
         </span>
       </button>
 
-      <div className="relative h-[20vh] w-full bg-[rgba(219, 239, 242, 1)] overflow-hidden">
+      <div className="relative h-[20vh] w-full bg-[rgba(219,239,242,1)] overflow-hidden">
         {/* SVG curve at the bottom */}
         <svg
           className="absolute bottom-0 left-0 w-full h-[100px]"
